6d3e60250cf323bc855d5207279a6927f3947f05
Hopper736-twink <mgordienks453@gmail.com>
2026-02-10 22:35:35 +0600

    Delete mafia.js

--- mafia.js
+++
@@ -1,7 +0,0 @@
-function mafiaMode(){
-screen.innerHTML=`<div class="card">
-<h2>Мафия</h2>
-<p>Роли раздаются автоматически</p>
-<button onclick="alert('Ночь начинается')">Начать</button>
-</div>`;
-}
